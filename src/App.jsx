df0ee11b89fc54773d709f3a9ad688e7bfed2481
Dipan Sarkar <alexmithun1005@gmail.com>
2024-05-05 21:51:18 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ const [steps, setSteps] = useState(1)
   const [count, setCount] = useState(0)
 
 
-  function minusSteps(){
-     setSteps(steps-1)
-   }
-   function plusSteps(){
-     setSteps(steps+1)
+  // function minusSteps(){
+  //    setSteps(steps-1)
+  //  }
+  //  function plusSteps(){
+  //    setSteps(steps+1)
      
-   }
+  //  }
  function minusCount(){
     setCount(count-steps)
 
@@ -28,18 +28,20 @@ const [steps, setSteps] = useState(1)
   }
   date.setDate(date.getDate()+count)
 
-  console.log(date)
-
+  
+console.log(steps)
+console.log(count)
 
  
 
 
   return (
     <>
-    <button onClick={minusSteps}>-</button> <span>steps :{steps} </span> <button onClick={plusSteps}>+</button>
+    <input type="range" min={0} max={10} value={steps} onChange={(e)=>setSteps(Number(e.target.value))} />
+    {/* <button onClick={minusSteps}>-</button> <span>steps :{steps} </span> <button onClick={plusSteps}>+</button> */}
      <br />
      <br />
-    <button onClick={minusCount}>-</button> <span>count :{count} </span>  <button onClick={plusCount}>+</button>
+    <button onClick={minusCount}>-</button> <input type="number" value={count} onChange={(e)=>setCount(Number(e.target.value))} />  <button onClick={plusCount}>+</button>
 
     <p>{count} days from today is  {date.toDateString()} </p>
 
