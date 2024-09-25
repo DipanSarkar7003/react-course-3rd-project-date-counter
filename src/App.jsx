b18ae9491d8777807e04af1aad0b88f3bf1072c3
Dipan Sarkar <alexmithun1005@gmail.com>
2024-09-25 21:15:46 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 
 import './App.css'
 
 function App() {
   const date = new Date("july 04 2024")
-  
+  const reduce = (state , action)=>{
+console.log(state.initCount, action)
+switch(action.type){
+ case "plus" : return {initCount : state.initCount+ action.payload }
+ case "minus" : return {initCount : state.initCount- action.payload }
+ case "settingVal" : return {initCount : action.payload }
+}
+}
+
+
+const initialState = {initCount : 0 }
   
 const [steps, setSteps] = useState(1)
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [count , dispatch] = useReducer(reduce , initialState)
 
 
   // function minusSteps(){
@@ -16,24 +27,30 @@ const [steps, setSteps] = useState(1)
   //  function plusSteps(){
   //    setSteps(steps+1)
      
-  //  }
+  //  }  
  function minusCount(){
-    setCount(count-steps)
-
+    // setCount(count-steps)
+dispatch( {type : "minus" , payload : 1 })
   
   }
   function plusCount(){
-    setCount(count+steps)
- 
+    // setCount(count+steps)
+dispatch({ type:"plus" , payload :  1})
   }
-  date.setDate(date.getDate()+count)
+  // date.setDate(date.getDate()+count)
 
   
-console.log(steps)
-console.log(count)
+// console.log(steps)
+// console.log(count)
 
+function settingVal(e){
+  dispatch( {type: "settingVal" , payload : Number(e.target.value)})
  
-
+}
+const updatedDate = new Date(new Date("july 04 2024"));
+updatedDate.setDate(updatedDate.getDate() + count.initCount);
+ 
+console.log(updatedDate)
 
   return (
     <>
@@ -41,9 +58,9 @@ console.log(count)
     {/* <button onClick={minusSteps}>-</button> <span>steps :{steps} </span> <button onClick={plusSteps}>+</button> */}
      <br />
      <br />
-    <button onClick={minusCount}>-</button> <input type="number" value={count} onChange={(e)=>setCount(Number(e.target.value))} />  <button onClick={plusCount}>+</button>
+    <button onClick={minusCount}>-</button> <input type="number" value={count.initCount} onChange={(e)=>settingVal(e)} />  <button onClick={plusCount}>+</button>
 
-    <p>{count} days from today is  {date.toDateString()} </p>
+    <p>{count.initCount} days from today is  {updatedDate.toDateString()} </p>
 
     </>
   )
